98cbd20c02319532dde73bacd40e8efe32b50e00
Ubuntu <ubuntu@jamie.novalocal>
2023-01-25 17:57:08 +0000

    testing updates to improve code

--- baffle.sh
+++ baffle.sh
@@ -9,13 +9,22 @@ subject=$2
 query_loc=$3
 output_directory=$4
 
+# check that input parameters were set.
+if [[ -z "${query}" ]] || [[ -z "${output_directory}" ]]
+ then echo "No input parameters were set - exiting..." && exit 1
+else # reformat directories to not include the ending hash if present
+  subject=$(echo "${subject}" | sed 's/\/$//')
+  output_directory=$(echo "${output_directory}" | sed 's/\/$//')
+fi
+
  # assert that tools are present and display versions in output
-echo "checking dependencies..."
+echo "checking dependency versions..."
 blastn -version
 makeblastdb -version
 blastdbcmd -version
 bedtools --version
-mafft --version
+echo "mafft $(mafft --version)"
+shopt -s extglob # this option needs to be on for correct file matching
 
  # make the output directory  - the script controls everything that goes in this directory, so it can be used for temporaty files too.
 if [[ -d "${output_directory}" ]]
@@ -25,30 +34,31 @@ fi
 
  # make the input multifasta for the blast database
 if [[ -f "${subject}" ]]
-  then cp "${subject}" "${output_directory}/db.fa"
- elif [[ -d "${subject}" ]]
-  then cat "${subject}"/*.{fasta,fa,fas,fna} >> "${output_directory}/db.fa"
-  for fasta in "${subject}"/*.{fasta,fa,fas,fna,ffn}.gz
-    do gunzip -c "${fasta}" >> "${output_directory}/db.fa"
+  then cp "${subject}" "${output_directory}/baffle_db"
+elif [[ -d "${subject}" ]] && [[ $(compgen -G "${subject}/*.@(fasta|fa|fas|fna|ffn)" | wc -l) -gt 0 ]]
+  then cat "${subject}"/*.@(fasta|fa|fas|fna|ffn) >> "${output_directory}/baffle_db"
+elif [[ -d "${subject}" ]] && [[ $(compgen -G "${subject}/*.@(fasta|fa|fas|fna|ffn)" | wc -l) -gt 0 ]]
+  then for fasta in "${subject}"/*.@(fasta|fa|fas|fna|ffn).gz
+    do gunzip -c "${fasta}" >> "${output_directory}/baffle_db"
   done
 else echo "Subject sequence or sequences not found: exiting..." && exit 1
 fi
  # make the blast database and move it to a specific directory
-makeblastdb -dbtype nucl -in "${output_directory}/db.fa" -parse_seqids
-mv "${output_directory}/${subject}."... "${output_directory}/blast_db"
+makeblastdb -dbtype nucl -in "${output_directory}/baffle_db" -parse_seqids
+mv "${output_directory}/baffle_db."??? "${output_directory}/blast_db"
 
   # moved the blastdb to a directory blast_db/
 blastn -db genomes_noST151/blast_db/genomes_noST151.fa -num_threads 8 -task 'blastn' -query "${query}" -query_loc "${query_loc}" -qcov_hsp_perc 20.0 -outfmt 6 -out "${output_directory}/${query}"_coords.tsv
 if [[ ! -f "${output_directory}/${query}"_coords.tsv ]]
   then echo 'BLAST failed - exiting...' && exit 1
-else awk -F'\t' '{print $1 "\t" $9-1 "\t" $10}' "${output_directory}/${query}"_coords.tsv > "${output_directory}/${query}"_coords.bed
-  bedtools --getfasta -fi "${output_directory}/db.fa" -bed "${output_directory}/${query}"_coords.bed -fo "${output_directory}/${query}_query.fasta"
-  rm -f "${output_directory}/db.fa" # the blast-db fasta is no longer needed - remove it!
+else awk -F'\t' '{print $2 "\t" $9-1 "\t" $10}' "${output_directory}/${query}"_coords.tsv > "${output_directory}/${query}"_coords.bed
+  bedtools getfasta -fi "${output_directory}/baffle_db" -bed "${output_directory}/${query}"_coords.bed -fo "${output_directory}/${query}_query.fasta"
+  rm -f "${output_directory}/baffle_db" # the blast-db fasta is no longer needed - remove it!
 fi
 
-if [[ ! -f "${output_directory}/${query}"_coords.tsv ]]
+if [[ ! -f "${output_directory}/${query}_query.fasta" ]]
   then echo 'Fasta files were not created successfully - exiting...' && exit 1
-else mafft --maxiterate 1000 --localpair "${output_directory}/${query}_query.fasta" > "${output_directory}/${query}_query.aln"
+else mafft --quiet --maxiterate 1000 --localpair "${output_directory}/${query}_query.fasta" > "${output_directory}/${query}_query.aln"
 fi
 
  # print confimration whether the alignment produced output
