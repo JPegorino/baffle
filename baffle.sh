90bb3e823e923edef4aa534b613db59dbf6636a2
JPegorino <jamie.gorzynski@ed.ac.uk>
2024-10-24 13:12:20 +0100

    adding upstream/downstream bump parameters

--- baffle.sh
+++ baffle.sh
@@ -20,6 +20,12 @@ while [ "$1" != "" ]; do
 		-l | --loci )		shift
 			query_loc=$1
 			;;
+		-us | --upstream )		shift
+			upstream_bump=$1
+			;;
+		-ds | --downstream )		shift
+			downstream_bump=$1
+			;;    
 		-t | --threads )	shift
 			threads=$1
 			;;
@@ -31,7 +37,7 @@ while [ "$1" != "" ]; do
       printf "checking dependencies...\n"
       blastn -version | head -1 || exit 1
       echo "mafft:" $(mafft --version 2>&1) || exit 1
-      seqtk 2>&1 | head -3 | tail -1 | sed 's/Version/seqtk/g'
+      seqtk 2>&1 | head -3 | tail -1 | sed 's/Version/seqtk/g' || exit 1
       seqkit version || exit 1
 			exit 0
       ;;
@@ -45,6 +51,8 @@ while [ "$1" != "" ]; do
 			printf "\n-o | --output_dir | output directory name - a directory where new files can be created to avoid overwriting originals. Default: baffle_${date_uid}"
 			printf "\n-t | --threads | number of threads to use. Default: 1"
       printf "\n-l | --loci | start and end loci in reference sequence to use for BLAST (in the format start-end). Default: Use entire sequence"
+      printf "\n-us | --upstream | sequence length (bp) before the start co-ordinate to include in the BLAST. Default: 0"
+      printf "\n-ds | --downstream | sequence length (bp) after the end co-ordinate to include in the BLAST. Default: 0"
 			printf "\n-r | --reverse_comp | flag to reverse complement the output. Default: off"
       printf "\n-v | --version | print version number, check depdencies and exit."
       printf "\n-h | --help | print this help page and exit."      
@@ -83,6 +91,20 @@ if [ -z "$threads" ]; then
   threads=1
 fi && echo "baffle will run tools using ${threads} threads."
 
+if [ -z "$upstream_bump" ]; then
+  upstream_bump=0
+elif [ $(echo ${upstream_bump} | fold -w1 | grep -vc "[0-9]") -gt 0 ] ; then
+  echo "ERROR: ${upstream_bump} - upstream value must be numeric."
+  exit 1
+fi
+
+if [ -z "$downstream_bump" ]; then
+  downstream_bump=0
+elif [ $(echo ${downstream_bump} | fold -w1 | grep -vc "[0-9]") -gt 0 ] ; then
+  echo "ERROR: ${downstream_bump} - downstream value must be numeric."
+  exit 1
+fi
+
 # correct likely delimiter typos in the query_loc parameter
 query_loc=$(echo ${query_loc} | sed 's/[,;/:]/-/g')
 if [ -z "$query_loc" ] ; then
@@ -93,6 +115,11 @@ elif [ $(echo ${query_loc} | fold -w1 | grep -vc "[0-9-]") -gt 0 ] ; then
   exit 1
 fi
 
+# add upstream and downstream bumps to the query loci
+query_start=$(( ${query_loc%-*} - ${upstream_bump} ))
+query_end=$(( ${query_loc#*-} + ${downstream_bump} ))
+query_loc="${query_start}-${query_end}"
+
 ## SPARE ##
 #if [ -z "$reverse_complement" ]; then
 #  reverse_complement=""
@@ -102,7 +129,7 @@ fi
 echo "checking dependencies are present..."
 blastn -version | head -1 || exit 1
 echo "mafft:" $(mafft --version 2>&1) || exit 1
-seqtk 2>&1 | head -3 | tail -1 | sed 's/Version/seqtk/g'
+seqtk 2>&1 | head -3 | tail -1 | sed 's/Version/seqtk/g' || exit 1
 seqkit version || exit 1
 shopt -s extglob # this option needs to be on for correct file matching
 echo "" # leave some space on the screen for neatness
@@ -114,28 +141,33 @@ else mkdir -v "${output_directory}" && mkdir "${output_directory}/blast_db"
 fi
 
  # make the input multi-fasta for the blast database
+cat "${query}" > "${output_directory}/blast_db/blast_db" # add the query sequence to the blast_db so it appears in the alignment
 if [[ -f "${subject}" ]]
-  then cp "${subject}" "${output_directory}/blast_db/blast_db"
+  then cat "${subject}" >> "${output_directory}/blast_db/blast_db"
 elif [[ -d "${subject}" ]] && [[ $(compgen -G "${subject}/*.@(fasta|fa|fas|fna|ffn)" | wc -l) -gt 0 ]]
   then cat "${subject}"/*.@(fasta|fa|fas|fna|ffn) >> "${output_directory}/blast_db/blast_db"
-elif [[ -d "${subject}" ]] && [[ $(compgen -G "${subject}/*.@(fasta|fa|fas|fna|ffn)" | wc -l) -gt 0 ]]
+elif [[ -d "${subject}" ]] && [[ $(compgen -G "${subject}/*.@(fasta|fa|fas|fna|ffn).gz" | wc -l) -gt 0 ]]
   then for fasta in "${subject}"/*.@(fasta|fa|fas|fna|ffn).gz
     do gunzip -c "${fasta}" >> "${output_directory}/blast_db/blast_db"
   done
 else echo "Subject sequence or sequences not found: exiting..." && exit 1
-fi && cp "${query}" >> "${output_directory}/blast_db/blast_db" 
-# if the db is made successfully, add the query sequence to the blast_db so it appears in the alignment
+fi && subject=$(basename "${subject}") # remove file path from subject variable - so it can be used in file names 
 
  # make the blast database and move it to a specific folder in the output directory
 makeblastdb -dbtype nucl -in "${output_directory}/blast_db/blast_db" -parse_seqids
 
   # run blast and convert the output to a fasta of the aligned regions with the query sequence
-blastn -db "${output_directory}/blast_db/blast_db" -num_threads "${threads}" -mt_mode 2 -task 'blastn' -query "${query}" -query_loc "${query_loc}" -outfmt 6 -out "${output_directory}/${subject}_baffle.coords.tsv" # a record of the unfiltered hits
+echo running BLAST for ${query} loci ${query_loc} with ${PWD} coverage cutoff.
+echo -e "QUERY\tSUBJECT\tPERC_IDENTITY\tALIGNED_LENGTH\tNUM_MISMATCHES\tNUM_GAP_OPENS\tQUERY_START\tQUERY_END\tSUBJECT_START\tSUBJECT_END\tE-VALUE\tBIT_SCORE"
+blastn -db "${output_directory}/blast_db/blast_db" -num_threads "${threads}" -mt_mode 2 -task 'blastn' -query "${query}" -query_loc "${query_loc}" -outfmt 6 -out "${output_directory}/${subject}_baffle.coords.tsv.tmp" # a record of the unfiltered hits
+echo -e "QUERY\tSUBJECT\tPERC_IDENTITY\tALIGNED_LENGTH\tNUM_MISMATCHES\tNUM_GAP_OPENS\tQUERY_START\tQUERY_END\tSUBJECT_START\tSUBJECT_END\tE-VALUE\tBIT_SCORE" > "${output_directory}/${subject}_baffle.coords.tsv"
+cat "${output_directory}/${subject}_baffle.coords.tsv.tmp" >> "${output_directory}/${subject}_baffle.coords.tsv" && rm "${output_directory}/${subject}_baffle.coords.tsv.tmp"
 blastn -db "${output_directory}/blast_db/blast_db" -num_threads "${threads}" -mt_mode 2 -task 'blastn' -query "${query}" -query_loc "${query_loc}" -qcov_hsp_perc 20.0 -max_hsps 1 -outfmt '6 sseqid sseq' | sed 's/^/>/' | tr '\t' '\n' > "${output_directory}/${subject}_baffle.fasta"
 if [[ ! -f "${output_directory}/${subject}_baffle.fasta" ]]
   then echo 'BLAST failed - exiting...' && exit 1
 else rm -rf "${output_directory}/blast_db" # the blast-db is no longer needed - remove it!
 fi
+
  # check that the fasta was generated and if so, create the alignment from the fasta
 if [[ ! -f "${output_directory}/${subject}_baffle.fasta" ]]
   then echo 'Fasta was not created successfully - exiting...' && exit 1
